189fcd23b87f7d8c627cc1881c6dae020a3af108
yangyang <yangyang@live.in>
2023-09-16 16:32:20 +0800

    feat: Add merge to reduce produce workload.

---
+++ merge.go
@@ -0,0 +1,81 @@
+package event
+
+import (
+	"sync"
+	"time"
+)
+
+// Merge merges many tasks with same key into one task in a tiny interval.
+type Merge interface {
+	// UpdateOnline provides change time.Duration on running time.
+	UpdateOnline(duration time.Duration)
+	// Allowed tells your goroutine to go on or return directly.
+	Allowed(key Key) bool
+	// Clear clears local storage of task keys.
+	Clear()
+	Run() error
+	Close()
+}
+
+type void struct{}
+
+var noop void
+
+type Key string
+
+type merge struct {
+	storage map[Key]void
+	ticker  *time.Ticker
+	mut     sync.RWMutex
+}
+
+var _ Merge = &merge{}
+
+func NewMerge(interval time.Duration) Merge {
+	return &merge{
+		storage: make(map[Key]void),
+		ticker:  time.NewTicker(interval),
+	}
+}
+
+func (m *merge) UpdateOnline(duration time.Duration) {
+	if duration < time.Millisecond {
+		return
+	}
+	m.ticker.Reset(duration)
+}
+
+func (m *merge) Allowed(key Key) bool {
+	m.mut.RLock()
+	_, has := m.storage[key]
+	m.mut.RUnlock()
+	if has {
+		return false
+	}
+
+	m.mut.Lock()
+	m.storage[key] = noop
+	m.mut.Unlock()
+
+	return true
+}
+
+func (m *merge) Clear() {
+	var newStorage = make(map[Key]void)
+	m.mut.Lock()
+	m.storage = newStorage
+	m.mut.Unlock()
+}
+
+func (m *merge) Run() error {
+	go func() {
+		for range m.ticker.C {
+			m.Clear()
+		}
+	}()
+	return nil
+}
+
+func (m *merge) Close() {
+	m.ticker.Stop()
+}
---
+++ merge_test.go
@@ -0,0 +1,44 @@
+package event
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+	"time"
+)
+
+func BenchmarkMerge(b *testing.B) {
+	merge := NewMerge(time.Millisecond * 500)
+	_ = merge.Run()
+	b.Cleanup(func() {
+		merge.Close()
+	})
+
+	var total = 0
+	var running = 0
+
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+		var k = Key(fmt.Sprint("task-", rand.Intn(20)))
+		total++
+		if i == 100 {
+			merge.UpdateOnline(time.Millisecond * 60)
+			b.Log("merge.UpdateOnline(time.Millisecond * 60)")
+		}
+		if i == 500 {
+			merge.Clear()
+			b.Log("merge.Clear()")
+		}
+		if i == 1000 {
+			merge.UpdateOnline(time.Millisecond * 200)
+			b.Log("merge.UpdateOnline(time.Millisecond * 200)")
+		}
+		b.StartTimer()
+		if merge.Allowed(k) {
+			running++
+		}
+	}
+
+	b.Logf("total: %d", total)
+	b.Logf("running: %d", running)
+}
